674eb919e09f443c6dd265caa4622378e004661b
prasad <prasad9055@gmail.com>
2022-04-13 11:29:06 +0530

    project setup iss successfull

---
+++ springwithrest/src/main/java/com/lazy/SpringwithrestApplication.java
@@ -0,0 +1,13 @@
+package com.lazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwithrestApplication.class, args);
+	}
+
+}
